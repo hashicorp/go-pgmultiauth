56c9ae2f9646fa005412e5d79c1351f1e5f76ed3
Tauhid <tauhidanjum@gmail.com>
2025-04-08 14:56:24 +0530

    common retry logic added

--- azure.go
+++ azure.go
@@ -8,26 +8,10 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/avast/retry-go/v4"
-	"github.com/hashicorp/go-hclog"
 )
 
-func getAzureAuthToken(clientID string, logger hclog.Logger) (*authToken, error) {
-	var token azcore.AccessToken
-
-	err := retry.Do(
-		func() error {
-			var err error
-			token, err = fetchAzureAuthToken(clientID)
-			return err
-		},
-		retry.Attempts(3),
-		retry.Delay(50*time.Millisecond),
-		retry.DelayType(retry.BackOffDelay),
-		retry.OnRetry(func(n uint, err error) {
-			logger.Error("failed to fetch azure token", "attempt", n, "error", err)
-		}),
-	)
+func getAzureAuthToken(clientID string) (*authToken, error) {
+	token, err := fetchAzureAuthToken(clientID)
 	if err != nil {
 		return nil, fmt.Errorf("fetching azure token: %v", err)
 	}
@@ -55,7 +39,7 @@ func fetchAzureAuthToken(clientID string) (azcore.AccessToken, error) {
 		Scopes: []string{"https://ossrdbms-aad.database.windows.net/.default"},
 	})
 	if err != nil {
-		return azcore.AccessToken{}, fmt.Errorf("getting token: %v", err)
+		return azcore.AccessToken{}, fmt.Errorf("getting token: %w", err)
 	}
 
 	return token, nil
